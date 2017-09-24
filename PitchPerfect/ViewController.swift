878970066159065f328713c001168a8ec8b5d0d8
Mr.X <mr.x@Rickys-MacBook-Pro.local>
2017-09-24 23:08:16 +0200

    type: feat
    body: Connected UI to Controller and added some icons

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(_ sender: UIButton) {
+        print("Button was pressed")
+        recordingLabel.text = "Recording in progress"
+    }
+    
+    @IBAction func stopRecording(_ sender: UIButton) {
+    }
+    
 }
 
